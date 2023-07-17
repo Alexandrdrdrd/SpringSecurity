3556482d9f5c100d7a39e5392a24d6acacaa14d5
Alexandrdrdrd <kands12349@gmail.com>
2023-07-17 19:03:46 +0300

    Logout

--- src/main/java/com/sasha/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/sasha/security/security/ApplicationSecurityConfig.java
@@ -3,7 +3,7 @@ package com.sasha.security.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -54,6 +55,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().rememberMe()
                 .tokenValiditySeconds(((int) TimeUnit.DAYS.toSeconds(21)))
                 .key("verySecureInfo")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me")
+                    .logoutSuccessUrl("/login")
         ;
     }
 
