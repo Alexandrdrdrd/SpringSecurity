4f381217bb83e9ed37b0f715b4faf4a614850a45
Alexandrdrdrd <kands12349@gmail.com>
2023-07-17 18:33:43 +0300

    Remember Me Cookie and Extra Options

--- src/main/java/com/sasha/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/sasha/security/security/ApplicationSecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.sasha.security.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.sasha.security.security.ApplicationUserRole.*;
 
@@ -50,6 +52,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses", true)
                 .and().rememberMe()
+                .tokenValiditySeconds(((int) TimeUnit.DAYS.toSeconds(21)))
+                .key("verySecureInfo")
         ;
     }
 
